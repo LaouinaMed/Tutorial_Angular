4bf0d72d05b458dd18b441e9c2bfff8d7d5d7ea6
Mohammed Laouina <simed.laouina@gmail.com>
2025-01-02 15:20:45 +0100

    create componennt roles and designation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RolesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

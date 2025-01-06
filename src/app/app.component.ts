520ae30743201d3e3fbb81dc921b5abaab764f01
Mohammed Laouina <simed.laouina@gmail.com>
2025-01-06 17:59:10 +0100

    create client and employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RolesComponent } from './components/roles/roles.component';
 import { MasterComponent } from './components/master/master.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MasterComponent],
+  imports: [CommonModule, RouterOutlet,MasterComponent,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/services/client.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../model/class/Client';
+import { environment } from 'src/environments/environment.development';
+import { APIResponseModel } from '../model/interface/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllClients(): Observable<APIResponseModel>{
+    return this.http.get<APIResponseModel>(environment.API_URL + "GetAllClients")
+  }
+
+  addUpdate(object :APIResponseModel): Observable<APIResponseModel>{
+    return this.http.post<APIResponseModel>(environment.API_URL + "GetAllClients",object)
+  }
+
+  deletClientById(id:number): Observable<APIResponseModel>{
+    return this.http.get<APIResponseModel>(environment.API_URL + "DeleteClientByClientId?clientId"+id)
+  }
+}
